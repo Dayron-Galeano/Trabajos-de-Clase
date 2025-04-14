8f794ecca073e617034a624f99dc32a6eabd918b
Dayron-Galeano <galeanomejiadayronstiven@gmail.com>
2025-04-14 16:49:44 -0500

    Update Punto 4.py

--- Taller 2/Punto 4.py
+++ Taller 2/Punto 4.py
@@ -1,8 +1,15 @@
+'''Se pregunta el número de casos y luego se pregunta para cada caso el número'''
 casos = int(input("Número de casos a procesar: "))
-for i in  range (casos):
+for i in  range (casos): 
     numero = int(input("Número: "))
+    '''Este if evalua si el número es mayor a 0 , menor a 2**31, y si el número no es un cuadrado
+    perfecto. Ya que se me hace ilogico hacer el procedimiento, sabiendo que el número que va a dar
+    es 1. Por ende, si el número escrito es un cuadrado perfecto, el programa no hára nada'''
     if numero > 0 and numero < 2**31 and int(numero**0.5) - numero**0.5 != 0:
         k = 1
+        '''Acá se verifica si número*k no es un cuadrado perfecto, si llega a serlo, se
+        acaba el while. Dando el menor número que multiplicado por "numero" de un cuadrado 
+        perfecto'''
         while int((numero*k)**0.5) - (numero*k)**0.5 != 0:
             k += 1
             resultadom = str(numero*k)
